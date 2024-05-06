1b86bb35eedf0ac5bed02dd619faf9117c417d35
lcurrie4 <lcurrie4@uncc.edu>
2024-05-06 19:55:13 -0400

    Revert "Merge Lucas2 Branch"
    This reverts commit 4bae3b8ae11e543311698bd63f4fa2cdc461efa4, reversing
    changes made to d859a221a2d69b3953304c2a5c41bc87e3b5e9aa.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -7,19 +7,15 @@ public class CharacterMovement : MonoBehaviour {
 	[SerializeField] private Transform trans;
 	[SerializeField] private Rigidbody rb;
 	[SerializeField] private Transform target;
-	[SerializeField] private Transform target1;
-	[SerializeField] private Transform target2;
-	[SerializeField] private Transform target3;
-	[SerializeField] private Transform target4;
 	[SerializeField] private Animator Animator;
-	
+
 	private float maxSpeed;
 	private float radiusOfSat;
 	private float detectionRadius;
 
 	void Start () {
 		maxSpeed = 3f;
-		radiusOfSat = 1f;
+		radiusOfSat = 2f;
 		detectionRadius = 35f;
 	}
 	
@@ -31,139 +27,19 @@ public class CharacterMovement : MonoBehaviour {
 		trans.rotation = Quaternion.LookRotation (towards);
 
 		// If we haven't reached the target yet
-		//Animator.SetBool("isMoving", false);
 		if (towards.magnitude > detectionRadius == false){
 		if (towards.magnitude > radiusOfSat) {
 
 			// Normalize vector to get just the direction
 			towards.Normalize ();
 			towards *= maxSpeed;
-			
+
 			// Move character
-			//Animator.SetBool("isMoving", true);
 			rb.velocity = towards;
 		} else {
-			
 			Animator.enabled = false;
 			this.enabled = false;
-			
-
-
-			
 		}
 		}
-		
-		// Calculate vector from character to target
-		Vector3 towards1 = target1.position - trans.position;
-		trans.rotation = Quaternion.LookRotation (towards);
-
-		// If we haven't reached the target yet
-		//Animator.SetBool("isMoving", false);
-		if (towards1.magnitude > detectionRadius == false){
-		if (towards1.magnitude > radiusOfSat) {
-
-			// Normalize vector to get just the direction
-			towards1.Normalize ();
-			//towards1 *= maxSpeed;
-			
-			// Move character
-			//Animator.SetBool("isMoving", true);
-			rb.velocity = towards1;
-		} else {
-			
-			Animator.enabled = false;
-			this.enabled = false;
-			
-		}
-		}
-
-
-
-
-				
-		// Calculate vector from character to target
-		Vector3 towards2 = target2.position - trans.position;
-		trans.rotation = Quaternion.LookRotation (towards);
-
-		// If we haven't reached the target yet
-		//Animator.SetBool("isMoving", false);
-		if (towards2.magnitude > detectionRadius == false){
-		if (towards2.magnitude > radiusOfSat) {
-
-			// Normalize vector to get just the direction
-			towards1.Normalize ();
-			//towards1 *= maxSpeed;
-			
-			// Move character
-			//Animator.SetBool("isMoving", true);
-			rb.velocity = towards2;
-		} else {
-			
-			Animator.enabled = false;
-			this.enabled = false;
-			
-		}
-		}
-
-
-
-
-				
-		// Calculate vector from character to target
-		Vector3 towards3 = target3.position - trans.position;
-		trans.rotation = Quaternion.LookRotation (towards);
-
-		// If we haven't reached the target yet
-		//Animator.SetBool("isMoving", false);
-		if (towards3.magnitude > detectionRadius == false){
-		if (towards3.magnitude > radiusOfSat) {
-
-			// Normalize vector to get just the direction
-			towards3.Normalize ();
-			//towards1 *= maxSpeed;
-			
-			// Move character
-			//Animator.SetBool("isMoving", true);
-			rb.velocity = towards3;
-		} else {
-			
-			Animator.enabled = false;
-			this.enabled = false;
-			
-		}
-		}
-
-
-
-
-				
-		// Calculate vector from character to target
-		Vector3 towards4 = target4.position - trans.position;
-		trans.rotation = Quaternion.LookRotation (towards);
-
-		// If we haven't reached the target yet
-		//Animator.SetBool("isMoving", false);
-		if (towards4.magnitude > detectionRadius == false){
-		if (towards4.magnitude > radiusOfSat) {
-
-			// Normalize vector to get just the direction
-			towards4.Normalize ();
-			//towards1 *= maxSpeed;
-			
-			// Move character
-			//Animator.SetBool("isMoving", true);
-			rb.velocity = towards4;
-		} else {
-			
-			Animator.enabled = false;
-			this.enabled = false;
-			
-		}
-		}
-
-
-
-
-		
 	}
 }
