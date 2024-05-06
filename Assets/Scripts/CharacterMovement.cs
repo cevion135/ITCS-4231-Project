c477290d725f764046f18c565a4203e7676857cb
lcurrie4 <lcurrie4@uncc.edu>
2024-05-06 17:16:35 -0400

    Zombie Script now targets all vehicles
    Zombies now collide with and move towards all vehicles

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -11,7 +11,6 @@ public class CharacterMovement : MonoBehaviour {
 	[SerializeField] private Transform target2;
 	[SerializeField] private Transform target3;
 	[SerializeField] private Transform target4;
-	[SerializeField] private Transform target5;
 	[SerializeField] private Animator Animator;
 	
 	private float maxSpeed;
@@ -48,7 +47,123 @@ public class CharacterMovement : MonoBehaviour {
 			Animator.enabled = false;
 			this.enabled = false;
 			
+
+
+			
 		}
 		}
+		
+		// Calculate vector from character to target
+		Vector3 towards1 = target1.position - trans.position;
+		trans.rotation = Quaternion.LookRotation (towards);
+
+		// If we haven't reached the target yet
+		//Animator.SetBool("isMoving", false);
+		if (towards1.magnitude > detectionRadius == false){
+		if (towards1.magnitude > radiusOfSat) {
+
+			// Normalize vector to get just the direction
+			towards1.Normalize ();
+			//towards1 *= maxSpeed;
+			
+			// Move character
+			//Animator.SetBool("isMoving", true);
+			rb.velocity = towards1;
+		} else {
+			
+			Animator.enabled = false;
+			this.enabled = false;
+			
+		}
+		}
+
+
+
+
+				
+		// Calculate vector from character to target
+		Vector3 towards2 = target2.position - trans.position;
+		trans.rotation = Quaternion.LookRotation (towards);
+
+		// If we haven't reached the target yet
+		//Animator.SetBool("isMoving", false);
+		if (towards2.magnitude > detectionRadius == false){
+		if (towards2.magnitude > radiusOfSat) {
+
+			// Normalize vector to get just the direction
+			towards1.Normalize ();
+			//towards1 *= maxSpeed;
+			
+			// Move character
+			//Animator.SetBool("isMoving", true);
+			rb.velocity = towards2;
+		} else {
+			
+			Animator.enabled = false;
+			this.enabled = false;
+			
+		}
+		}
+
+
+
+
+				
+		// Calculate vector from character to target
+		Vector3 towards3 = target3.position - trans.position;
+		trans.rotation = Quaternion.LookRotation (towards);
+
+		// If we haven't reached the target yet
+		//Animator.SetBool("isMoving", false);
+		if (towards3.magnitude > detectionRadius == false){
+		if (towards3.magnitude > radiusOfSat) {
+
+			// Normalize vector to get just the direction
+			towards3.Normalize ();
+			//towards1 *= maxSpeed;
+			
+			// Move character
+			//Animator.SetBool("isMoving", true);
+			rb.velocity = towards3;
+		} else {
+			
+			Animator.enabled = false;
+			this.enabled = false;
+			
+		}
+		}
+
+
+
+
+				
+		// Calculate vector from character to target
+		Vector3 towards4 = target4.position - trans.position;
+		trans.rotation = Quaternion.LookRotation (towards);
+
+		// If we haven't reached the target yet
+		//Animator.SetBool("isMoving", false);
+		if (towards4.magnitude > detectionRadius == false){
+		if (towards4.magnitude > radiusOfSat) {
+
+			// Normalize vector to get just the direction
+			towards4.Normalize ();
+			//towards1 *= maxSpeed;
+			
+			// Move character
+			//Animator.SetBool("isMoving", true);
+			rb.velocity = towards4;
+		} else {
+			
+			Animator.enabled = false;
+			this.enabled = false;
+			
+		}
+		}
+
+
+
+
+		
 	}
 }
